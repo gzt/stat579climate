5c4c77f15d5076d907e0fc6b485ea1b855569ae7
g z thompson <gzthompson@gmail.com>
2012-11-11 17:52:38 -0600

    new commit

--- install.r
+++ install.r
@@ -1,3 +1,4 @@
 install.packages("ncdf4", dependencies=TRUE)
 install.packages("ncdf", dependencies=TRUE)
 #I could only get the old one to work
+#and even then I never tested it. I think we'll work with the ASCII
